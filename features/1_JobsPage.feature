1a3bcf3a7dfe1c637ee457679710f6e3b0f07a90
alphanash <nyasham@gmail.com>
2019-09-01 14:36:23 +0100

    1_JobsPage- Completed navigation gherkin and and linked to .js (blank)

--- features/1_JobsPage.feature
+++ features/1_JobsPage.feature
@@ -3,3 +3,8 @@ Feature: Jobs page
   Scenario: Navigation bar renders
     Given I go to the jobs page
     Then I should see the navigation bar
+    And I should see the search fields
+    And I should see the sector lists
+    And I should see the jobs blog
+    And I should see the featured jobs
+    And I should see the footer
--- features/step_definitions/1_JobsPage.js
+++ features/step_definitions/1_JobsPage.js
@@ -7,3 +7,19 @@ Given(/^I go to the jobs page$/, () => World.goToJobsPage());
 Then(/^I should see the navigation bar$/, async () => {
   return World.driver.findElement(By.id('primary-nav'));
 })
+
+Then(/^I should see the search fields$/, function () {
+
+});
+Then(/^I should see the sector lists$/, function () {
+
+});
+Then(/^I should see the jobs blog$/, function () {
+
+});
+Then(/^I should see the featured jobs$/, function () {
+
+});
+Then(/^I should see the footer$/, function () {
+
+});
