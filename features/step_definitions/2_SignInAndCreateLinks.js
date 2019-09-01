3ed245c5b9e93209533dacb10592d17d54632598
alphanash <nyasham@gmail.com>
2019-09-01 23:47:04 +0100

    2_SignInAndCreateLinks Unable to get .click() to work. I know it is bad practice to commit non-working code, but I am going to call it a day and request assistance

--- features/step_definitions/2_SignInAndCreateLinks.js
+++ features/step_definitions/2_SignInAndCreateLinks.js
@@ -1,4 +1,37 @@
-const {Given, Then} = require('cucumber');
+const {Given, Then, When} = require('cucumber');
 const {By} = require('selenium-webdriver');
 const World = require('../support/world');
 
+Given(/^I am not signed\-in$/, function () {
+try
+{
+    //Looks for the "sign Out" element. If it exists, then the browser cookies are cleared then refreshed.
+    //This is obviously a non-test as the test browser always starts with an empty cache... I'm just putting this here to show that I know how to build for such a scenario (and potentially use it the other way around).
+    const SignOut = require (World.driver.findElement(By.css('#secondary-nav > ul > li.togglable-nav__item.secondary-nav__item.secondary-nav--jobseekers.jobseekers.jobseekers-nav > ul > li.togglable-nav__item.togglable-nav__item--last.jobseekers__item.jobseekers__item--sign-out')));
+    if(SignOut(true))
+    {
+        // This call doesn't seem to work for some reason. SO instead of clearing cookies and refreshing the page, I'll just use the webdriver to click "sign out". Less robust and I'm not happy about it, but it's what I can do in the time available.
+        // World.driver.manage().deleteAllCookies();
+        World.driver.findElement(By.css('#secondary-nav > ul > li.togglable-nav__item.secondary-nav__item.secondary-nav--jobseekers.jobseekers.jobseekers-nav > ul > li.togglable-nav__item.togglable-nav__item--last.jobseekers__item.jobseekers__item--sign-out')).click();
+    }}
+    catch (NoSuchElementException)
+    {
+    }
+});
+
+
+When(/^I click on (.*)$/, function (link) {
+    const SignIn = '#secondary-nav > ul > li.togglable-nav__item.secondary-nav__item.secondary-nav--jobseekers.jobseekers.jobseekers-nav > ul > li.togglable-nav__item.jobseekers__item.jobseekers__item--sign-in\n';
+    const CreateAccount = '#secondary-nav > ul > li.togglable-nav__item.secondary-nav__item.secondary-nav--jobseekers.jobseekers.jobseekers-nav > ul > li.togglable-nav__item.togglable-nav__item--last.jobseekers__item.jobseekers__item--create-account';
+
+    World.driver.findElement(By.css(link).click());
+});
+
+
+Then(/^I am taken to the (.*) page$/, function (page) {
+    const Logon = '.../logon/';
+    const Register = '.../register/';
+
+    return World.driver.getCurrentUrl();
+    console.assert(currenturl, page );
+});
